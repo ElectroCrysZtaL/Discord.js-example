f083a844a76e4e820e559141bf63878a14448818
ElectroCrysZtaL <41235770+ElectroCrysZtaL@users.noreply.github.com>
2018-08-26 20:27:14 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,6 @@
+const Discord = require('discord.js')
+
 exports.run = (client, message, args) => {
   const m = await message.channel.send("pinging...")
-  m.edit(`Pong! ${m.createdTimestamp - message.createdTimestamp}ms.`)
+  m.edit(`Pong! ${m.createdTimestamp - message.createdTimestamp}ms. Made by @ElectroCrysZtaL#0001`)
 }
