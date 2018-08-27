850f7c0ab6be7d93de9cd2d52497142fe325a4ce
ElectroCrysZtaL <41235770+ElectroCrysZtaL@users.noreply.github.com>
2018-08-27 09:21:25 +0700

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+
+exports.run(client, message, args) {
+ const user = message.mentions.users.first();
+ // The mentioned user.
+ member.ban();
+ // Bans the user
+ console.log(`${member.user.tag} was banned!`)
+ // Logs when the user is banned.
+}
