bb65ba302cf297e1d405d1d077802f2d72921065
ElectroCrysZtaL <41235770+ElectroCrysZtaL@users.noreply.github.com>
2018-08-27 09:21:48 +0700

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-exports.run(client, message, args) {
+exports.run = (client, message, args) => {
  const user = message.mentions.users.first();
  // The mentioned user.
  member.ban();
